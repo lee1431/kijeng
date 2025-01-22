1e265b78a7a30423b7fe34d7d13ba251e4e91629
lee1431 <37124343+lee1431@users.noreply.github.com>
2025-01-22 19:49:46 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 async function fetchNotices() {
-    const url = "https://api.github.com/repos/lee1431/web/contents/notices.json";
+    const url = "https://api.github.com/repos/lee1431/gear/contents/notices.json";
 
     try {
         const response = await fetch(url, {
